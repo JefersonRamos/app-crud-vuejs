82d88e89373d2641999756604e5e20cc3541270a
JefersonRamos <jefersonramosoliveira8@gmail.com>
2021-07-16 10:36:11 -0300

    Starting vueJs project

---
+++ app-crud-vuejs/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0px;
+}
+</style>
